5f37e88d8ad60d2912f19181c08499df8b56d8fe
Lucas Pedreira <pedreiralucasm@gmail.com>
2022-11-01 20:22:59 -0300

    first navBar

---
+++ src/papelera/cosas.jsx
@@ -0,0 +1,39 @@
+// import React from "react";
+// import { Box, AppBar, Toolbar, Container, Grid, Button, Typography, Stack } from "@mui/material";
+
+
+// export const NavBar: React.FC<{}> = () => {
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//       <AppBar position="fixed">
+//         <Toolbar>
+//           <Container maxWidth="xl">
+//             <Grid
+//               container
+//               direction="row"
+//               justifyContent="space-between"
+//               alignItems="Center"
+//             >
+//               <Grid item>
+                
+//               </Grid>
+//               <Grid item>
+//                 <Stack direction= "row" spacing= {2}>
+//                 <Button variant= "contained">
+//                     Descubrí Global Evolution
+//                 </Button>
+//                 <Button variant= "contained">
+//                     Contacto
+//                 </Button>
+//                 <Button variant= "contained">
+//                     Quienes somos?
+//                 </Button>
+//                 </Stack>
+//               </Grid>
+//             </Grid>
+//           </Container>
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//   );
+// };
