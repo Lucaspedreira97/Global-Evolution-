b7e64f2d31c022ab6afefc3812786f093f470b8a
Lucas Pedreira <102390738+Lucaspedreira97@users.noreply.github.com>
2022-11-08 17:18:19 -0300

    Revert "Dev"

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,12 @@
 import { Box, Button, Container } from "@mui/material";
 import React from "react";
-import MediaCard from "../../components/card";
 import { HeaderComponent } from "../../Headers";
-
 
 
 export const HomePage: React.FC<{}> = () => {
   return (
     <Container sx={{mt: 9}} maxWidth="xl">
      <HeaderComponent title="Descubrí todo lo que podés hacer desde la APP"/>
-     <MediaCard/>
     </Container>
   );
 };
